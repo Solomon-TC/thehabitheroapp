bfec374ae01509dac6c253138dd98e30e9960af5
capell.solomon <capell.solomon@gmail.com>
2024-12-05 21:34:49 +0000

    Update configuration files

--- next.config.js
+++ next.config.js
@@ -1,25 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['uyyvxnlivatysbplzhwz.supabase.co'],
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ]
+  images: {
+    domains: ['localhost'],
   },
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false }
-    return config
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
   },
 }
